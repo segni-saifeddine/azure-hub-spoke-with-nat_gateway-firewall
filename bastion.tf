7ba39311e131eaa6456f0b2235fcad6e534e7cfa
SAIFEDDINE SEGNI <63507313+segni-saifeddine@users.noreply.github.com>
2024-11-05 12:54:38 +0100

    add bastion.tf files

---
+++ bastion.tf
@@ -0,0 +1,19 @@
+resource "azurerm_public_ip" "bastion" {
+  name                = "pip-bastion"
+  location            = azurerm_resource_group.hub.location
+  resource_group_name = azurerm_resource_group.hub.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "bastion" {
+  name                = "bastion"
+  location            = azurerm_resource_group.hub.location
+  resource_group_name = azurerm_resource_group.hub.name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.bastion.id
+    public_ip_address_id = azurerm_public_ip.bastion.id
+  }
+}
